9d694ff9494c139817e24cd08b879d960e99aa08
esteban launay <etochy@gmail.com>
2014-10-13 16:12:22 +0200

    ptit change

--- src/com/example/screen2screen/ConnectionActivity.java
+++ src/com/example/screen2screen/ConnectionActivity.java
@@ -2,42 +2,50 @@ package com.example.screen2screen;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ExpandableListView;
 
-public class ConnectionActivity extends Activity{
-	
-	public ExpandableListView list;
-	public Button conn;
-	public Button retour;
+public class ConnectionActivity extends Activity implements OnClickListener{
+
+	private ExpandableListView list;
+	private Button conn;
+	private Button buttonReturn;
+	private Intent intent;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.connection_activity);
-	
-			list = (ExpandableListView) findViewById(R.id.list);
-			conn = (Button) findViewById(R.id.buttonConn);
-			retour = (Button) findViewById(R.id.buttonReturn);
-			
-	       
-	        list.setGroupIndicator(null);
-	        list.setChildIndicator(null);
-	        
-	        String[] titles = {"Téléphone Esteban","Téléphone B","Téléphone C"};
-	        String[] fruits = {"a1","a2"};
-	        String[] veggies = {"b1","b2","b3"};
-	        String[] meats = {"c1","c2"};
-	        String[][] contents = {fruits,veggies,meats};
-	        
-	        MyExpandableAdapter adapter = new MyExpandableAdapter(this,titles,contents);
 
-	        list.setAdapter(adapter);
-	        
-	       
+		list = (ExpandableListView) findViewById(R.id.list);
+		conn = (Button) findViewById(R.id.buttonConn);
+		buttonReturn = (Button)findViewById(R.id.buttonReturn);
+
 		
+
+
+		list.setGroupIndicator(null);
+		list.setChildIndicator(null);
+
+		String[] titles = {"Téléphone Esteban","Téléphone B","Téléphone C"};
+		String[] fruits = {"a1","a2"};
+		String[] veggies = {"b1","b2","b3"};
+		String[] meats = {"c1","c2"};
+		String[][] contents = {fruits,veggies,meats};
+
+		MyExpandableAdapter adapter = new MyExpandableAdapter(this,titles,contents);
+
+		list.setAdapter(adapter);
+
+		buttonReturn.setOnClickListener(this);
+
+
 	}
 
 	@Override
@@ -57,6 +65,20 @@ public class ConnectionActivity extends Activity{
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.buttonReturn:
+			ouvrirAccueil();
+			System.exit(0);
+			break;
+		}
+	}
+	private void ouvrirAccueil() {
+		intent = new Intent(this, AccueilActivity.class);
+		startActivity(intent);
 	}
 }
 
