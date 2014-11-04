f683b1ccd1739c42533619add24dc1392a4bca3b
esteban launay <etochy@gmail.com>
2014-11-04 11:40:45 +0100

    logo

--- gen/com/example/screen2screen/R.java
+++ gen/com/example/screen2screen/R.java
@@ -25,7 +25,7 @@ public final class R {
         public static final int bouton=0x7f020000;
         public static final int boutonbase=0x7f020001;
         public static final int boutonclique=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
+        public static final int iconeappli=0x7f020003;
         public static final int liste=0x7f020004;
         public static final int logo=0x7f020005;
         public static final int text=0x7f020006;
